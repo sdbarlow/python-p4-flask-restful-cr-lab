bb3aa1f1ed9efd1b3179d5fbd36d4412ffc435a7
sdbarlow <sethdbarlow@gmail.com>
2023-04-05 08:53:24 -0600

    alltests pass

--- server/models.py
+++ server/models.py
@@ -6,4 +6,7 @@ db = SQLAlchemy()
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Integer)
     id = db.Column(db.Integer, primary_key=True)
